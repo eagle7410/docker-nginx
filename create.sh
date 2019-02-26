6ab631a54115a28699fa6b47760253ab39058c57
Игорь (JS) <igor.js@hitech18.online>
2019-02-26 12:11:25 +0200

    More.

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name docker-nginx  -p 8080:80 -v /home/igor/nginx-config/nginx.conf:/etc/nginx/nginx.conf -d nginx
+docker run --name docker-nginx  -p 8080:80 -v ./nginx.conf:/etc/nginx/nginx.conf -d nginx
